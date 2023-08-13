75b07f9fefc85786bc28c45a9ad430d7670647e7
msuad <1445617868@qq.com>
2023-08-14 02:03:50 +0800

    feat<PA0>:Íê³ɡ°½ÓÈëNVBoard¡± (Mon Aug 14 02:03:50 CST 2023)

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,96 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
+#include <nvboard.h>
+#include <Vtop.h>
 
-// For std::unique_ptr
-#include <memory>
+static TOP_NAME dut;
 
-// Include common routines
-#include <verilated.h>
+void nvboard_bind_all_pins(Vtop* top);
 
-// Include model header, generated from Verilating "top.v"
-#include "Vtop.h"
+static void single_cycle() {
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
+}
 
-// Legacy function required only so linking works on Cygwin and MSVC++
-double sc_time_stamp() { return 0; }
+static void reset(int n) {
+  dut.rst = 1;
+  while (n -- > 0) single_cycle();
+  dut.rst = 0;
+}
 
-int main(int argc, char **argv)
-{
-    // This is a more complicated example, please also see the simpler examples/make_hello_c.
+int main() {
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
 
-    // Prevent unused variable warnings
-    if (false && argc && argv)
-    {
-    }
+  reset(10);
 
-    // Create logs/ directory in case we have traces to put under it
-    Verilated::mkdir("logs");
-
-    // Construct a VerilatedContext to hold simulation time, etc.
-    // Multiple modules (made later below with Vtop) may share the same
-    // context to share time, or modules may have different contexts if
-    // they should be independent from each other.
-
-    // Using unique_ptr is similar to
-    // "VerilatedContext* contextp = new VerilatedContext" then deleting at end.
-    const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
-    // Do not instead make Vtop as a file-scope static variable, as the
-    // "C++ static initialization order fiasco" may cause a crash
-
-    // Set debug level, 0 is off, 9 is highest presently used
-    // May be overridden by commandArgs argument parsing
-    contextp->debug(0);
-
-    // Randomization reset policy
-    // May be overridden by commandArgs argument parsing
-    contextp->randReset(2);
-
-    // Verilator must compute traced signals
-    contextp->traceEverOn(true);
-
-    // Pass arguments so Verilated code can see them, e.g. $value$plusargs
-    // This needs to be called before you create any model
-    contextp->commandArgs(argc, argv);
-
-    // Construct the Verilated model, from Vtop.h generated from Verilating "top.v".
-    // Using unique_ptr is similar to "Vtop* top = new Vtop" then deleting at end.
-    // "TOP" will be the hierarchical name of the module.
-    const std::unique_ptr<Vtop> top{new Vtop{contextp.get(), "TOP"}};
-
-    // Set Vtop's input signals
-    top->a = 0;
-    top-> b = 0;
-
-
-    // Simulate until $finish
-    while (!contextp->gotFinish())
-    {
-
-        contextp->timeInc(1); // 1 timeprecision period passes...
-
-
-        int a = rand() & 1;
-        int b = rand() & 1;
-        top->a = a;
-        top->b = b;
-        top->eval();
-        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-        assert(top->f == (a ^ b));
-
-        static int cnt=0;
-        if(cnt++>=30) break;
-    }
-
-    // Final model cleanup
-    top->final();
-
-    // Coverage analysis (calling write only after the test is known to pass)
-#if VM_COVERAGE
-    Verilated::mkdir("logs");
-    contextp->coveragep()->write("logs/coverage.dat");
-#endif
-
-    // Return good completion status
-    // Don't use exit() or destructor won't get called
-    return 0;
+  while(1) {
+    nvboard_update();
+    single_cycle();
+  }
 }
--- utils/git_push.sh
+++ utils/git_push.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-
+ysyx_stage="PA0"
 PROXY_SRC=./utils/util_proxy_env.sh
 
 t=`date`
-COMMIT_INFO="script commit: ${t}"
+# COMMIT_INFO="script commit: ${t}"
 # COMMIT_INFO="first commit"
+COMMIT_INFO="feat<${ysyx_stage}>:完成“接入NVBoard” (${t})"
+# COMMIT_INFO="docs<${ysyx_stage}>:"
+# COMMIT_INFO="test<${ysyx_stage}>:"
+# COMMIT_INFO="fix<${ysyx_stage}>:"
 
 
 sh ${PROXY_SRC}
